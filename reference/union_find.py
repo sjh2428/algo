c16a72073735d1e01ddcfdeb1125c211db18b6c9
Seong <pok_gare@naver.com>
2020-02-10 17:11:24 +0900

    union find reference 추가

---
+++ reference/union_find.py
@@ -0,0 +1,24 @@
+# 부모 노드를 찾는 함수
+def find_parent(parent, x):
+    if parent[x] == x:
+        return x
+    parent[x] = find_parent(parent, parent[x])
+
+
+# 두 부모 노드를 합치는 함수
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+# 부모가 같은가
+def is_equal_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a == b:
+        return True
+    return False
