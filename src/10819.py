edd81c2175cae5ae233be0aaac48b807814fe77a
Seong <pok_gare@naver.com>
2020-03-11 17:01:27 +0900

    10819 차이를 최대로 - 순열

---
+++ src/10819.py
@@ -0,0 +1,49 @@
+# https://www.acmicpc.net/problem/10819
+
+
+def next_permutation(perm):
+    n = len(perm)
+    i = n - 1
+    while i > 0 and perm[i - 1] >= perm[i]:
+        i -= 1
+
+    if i <= 0:
+        return False
+
+    j = n - 1
+    while perm[j] <= perm[i - 1]:
+        j -= 1
+    perm[i - 1], perm[j] = perm[j], perm[i - 1]
+
+    k = n - 1
+    while i < k:
+        perm[i], perm[k] = perm[k], perm[i]
+        i += 1
+        k -= 1
+
+    return True
+
+
+def calc(l):
+    ans = 0
+    for i in range(len(l) - 1):
+        ans += abs(l[i] - l[i + 1])
+    return ans
+
+
+def solution():
+    ans = 0
+    N = int(input())
+    l = list(map(int, input().split()))
+    l.sort()
+
+    while True:
+        result = calc(l)
+        ans = max(ans, result)
+        if not next_permutation(l):
+            break
+
+    print(ans)
+
+
+solution()
