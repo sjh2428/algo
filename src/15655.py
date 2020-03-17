a891318644c85a0c53a8573ba49196d4e4f6f833
Seong <pok_gare@naver.com>
2020-03-17 18:22:47 +0900

    15655 - N과 M(6)

---
+++ src/15655.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/15655
+
+
+def go(idx, start, ans, N, M, nums):
+    if idx == M:
+        for a in ans:
+            print(a, end=" ")
+        print()
+        return
+    for i in range(start, N + 1):
+        ans[idx] = nums[i - 1]
+        go(idx + 1, i + 1, ans, N, M, nums)
+
+
+def solution():
+    N, M = map(int, input().split())
+    nums = list(map(int, input().split()))
+    nums.sort()
+    ans = [0 for _ in range(M)]
+    go(0, 1, ans, N, M, nums)
+
+
+solution()
