b225b18957529e2faa3e4473a28eb78a2ed61123
Seong <pok_gare@naver.com>
2020-09-09 23:21:00 +0900

    1149 - temp

---
+++ src/1149.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/1149
+
+from sys import stdin
+
+
+def solution():
+    N = int(stdin.readline())
+    arr = []
+    for _ in range(N):
+        arr.append(list(map(int, stdin.readline().split())))
+
+
+
+solution()
