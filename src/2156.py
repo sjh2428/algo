c45b7404255f6a04792357194564cdfa663f7a21
sjh2428 <pok_gare@naver.com>
2020-09-18 12:04:58 +0900

    2156 포도주 시식 - DP

--- src/2156.py
+++ src/2156.py
@@ -1,25 +1,26 @@
 # https://www.acmicpc.net/problem/2156
 
+from sys import stdin
+
 
 def solution():
-    n = int(input())
+    N = int(stdin.readline())
     amount = []
-    dp = []
-    for _ in range(n):
-        a = int(input())
-        amount.append(a)
-    dp.append(amount[0])
-    if n >= 2:
-        dp.append(amount[0] + amount[1])
-    if n >= 3:
-        dp.append(max(dp[1], dp[0] + amount[2], amount[1] + amount[2]))
-    if n >= 4:
-        for i in range(3, n):
-            dp.append(max(dp[i - 1],
-                          dp[i - 2] + amount[i],
-                          dp[i - 3] + amount[i - 1] + amount[i]))
+    dp = [0 for _ in range(N)]
+    for _ in range(N):
+        amount.append(int(stdin.readline()))
+    dp[0] = amount[0]
+    if N >= 2:
+        dp[1] = dp[0] + amount[1]
+    if N >= 3:
+        dp[2] = max(dp[1], dp[0] + amount[2], amount[1] + amount[2])
+    if N >= 4:
+        for i in range(3, N):
+            dp[i] = max(dp[i - 1],
+                        dp[i - 2] + amount[i],
+                        dp[i - 3] + amount[i - 1] + amount[i])
 
-    print(dp[n - 1])
+    print(dp[N - 1])
 
 
 solution()
