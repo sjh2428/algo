ec57acffe2c4753de534f570e9b47708eda32781
Seong <pok_gare@naver.com>
2020-09-25 14:51:08 +0900

    2309 일곱 난쟁이 - 완탐

--- src/2309.py
+++ src/2309.py
@@ -1,18 +1,23 @@
+# https://www.acmicpc.net/problem/2309
+
+from sys import stdin
+
+
 def find_lier(total, smalls):
     for i in range(len(smalls) - 1):
         for j in range(i + 1, len(smalls)):
             if total - (smalls[i] + smalls[j]) == 100:
                 return [smalls[i], smalls[j]]
 
-smalls = []
-for _ in range(9):
-    small = int(input())
-    smalls.append(small)
 
-smalls.sort()
-total = sum(smalls)
-lier_list = find_lier(total, smalls)
-for small in smalls:
-    if small in lier_list:
-        continue
-    print(small)
+def solution():
+    smalls = [int(stdin.readline()) for _ in range(9)]
+    smalls.sort()
+    total = sum(smalls)
+    lier_list = find_lier(total, smalls)
+    for small in smalls:
+        if small not in lier_list:
+            print(small)
+
+
+solution()
